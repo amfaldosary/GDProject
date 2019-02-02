0bd35c2bf1c9ff4048a2e31c6514d2cada488583
ABDULRAHMAN ALDOSARY <amfaldosary@gmail.com>
2019-02-02 22:07:33 +0300

    added maps

--- AppComponents/Home/Home.js
+++ AppComponents/Home/Home.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Styles from './../../StyleSheet';
-
+import MapView from 'react-native-maps';
 export default class Login extends React.Component {
   static navigationOptions = {
     title: 'Home',
   };
   render() {
     return (
-      <View style={Styles.container}>
-       <Text style={Styles.textFont}> Home screen SUCKERS! </Text> 
+      <View style={styles.container} >
+          <MapView
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        </MapView>
       </View>
     );
   }
@@ -17,9 +26,13 @@ export default class Login extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject,
+    height: 600,
+    width: 400,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+ });
